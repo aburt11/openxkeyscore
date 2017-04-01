76fbae001d884af271a6c7390856978ef8b52ec6
aburt11 <aburton.live@gmail.com>
2017-04-02 05:11:10 +1000

    tweaks

---
+++ www/server/server.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var PeerServer = require('peer').PeerServer;
+var server = PeerServer({port: 9000, path: '/myapp', proxied: true,
+  ssl: {
+    key: fs.readFileSync('/path/to/your/ssl/key/here.key'),
+    cert: fs.readFileSync('/path/to/your/ssl/certificate/here.crt')
+  }
+});
